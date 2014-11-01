178a5073644a64a2668dd14be29994047ebd8c36
Cyrus <cyrus1127@gmail.com>
2014-11-01 08:40:11 +0800

    creating game element

--- scripts/main.js
+++ scripts/main.js
@@ -21,20 +21,41 @@ requirejs(['libs/jquery-1.9.1.min', 'libs/TweenMax.min','main'], function(util) 
             initPage : function(){
                 var msg = "doing initial .....";
                     funs.debug._onScreen(msg);
-            }
-            ,
+            },
+            game : {
+                self :
+                {
+                    h : $("#dollar"),
+                },
+                _control_up : function(){
+                    var cur_y = funs.game.self.h.css('top').split('px')[0];
+                    var cur_x = funs.game.self.h.css('left').split('px')[0];
+
+                    funs.debug._onScreen( "Pos : " + cur_x + ", " + cur_y );
+                },
+                _control_down : function(){  },
+                _start : function(){},
+                _stop : function(){},
+                _anim : {
+                    _self_normal : function(){
+                        var self = $("#dollar");
+                        TweenMax.moveTo();
+                    },
+                }
+            },
             debug : {
                 _console : function( msg ){
                     console.log(msg);
-                }
-                ,
+                },
                 _onScreen : function( msg ){
                     $('<div class="log_msg" >'+msg+'</div>').appendTo($('#console_board'));
                     funs.debug._console(msg);
                 }
             }
         };
+
         funs.initPage();
+        funs.game._control_up();
     });
 
 });
